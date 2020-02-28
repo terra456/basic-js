6bfe0c8c81c82ffc32db12cab725da6ecf6aabde
Olga Bainova (nb) <terra.456@yandex.ru>
2020-02-29 02:12:52 +0500

    feat: implement the countCats(backyard) function that count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,24 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(arr) {
+  
   // remove line with error and write your code here
+  var quantityOfCats = 0;
+  if (!arr || arr.length == 0) {
+    return 0;
+  } else {
+    var newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      
+      if (Array.isArray(element)) {
+        newArr = newArr.concat(element);        
+      }    
+    }
+    for (let j = 0; j < newArr.length; j++) {
+      const newElement = newArr[j];
+      if (newElement == "^^") {
+        quantityOfCats = quantityOfCats+1;
+      }
+    }
+  }
+return quantityOfCats;
 };
